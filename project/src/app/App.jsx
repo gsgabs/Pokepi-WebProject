acb5bfff7f89cce29f8d8ef0d05183f49228e35d
Gabriel <135984152+gsgabs@users.noreply.github.com>
2023-11-06 21:37:55 -0300

    0.0.0.2v
    adição da barra de navegação

--- project/src/app/App.jsx
+++ project/src/app/App.jsx
@@ -3,7 +3,19 @@ import './App.css';
 export default function App() {
   return (
     <>
-      <h1>cachoeira</h1>
+      <nav className='BarraDenav'>
+        <ul type="none" className='gerasons'>
+          <li>Kanto - Gen 1</li>
+          <li>Johto - Gen 2</li>
+          <li>Hoenn - Gen 3</li>
+        </ul>
+      </nav>
+      <div>
+        Barra de pesquisa
+      </div>
+      <div id="body">
+        Pokedex
+      </div>
     </>
   );
 } 
