af22774fa6a117a7d7f05a5262ad9164b71f77d0
AndreVsc <vsandre40@gmail.com>
2023-11-09 14:58:16 -0300

    0.0.0.7v
    Update names status poekmon API

--- project/src/pages/Pokemon.jsx
+++ project/src/pages/Pokemon.jsx
@@ -5,76 +5,133 @@ import { useParams } from 'react-router-dom';
 function Pokemon() {
   const { id } = useParams();
   const [data, setData] = useState({});
-  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
-  
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await apFetch.get(`pokemon-form/${id}`);
-        setData(res.data);
+        const [pokemonResponse, statusResponse] = await Promise.all([
+          apFetch.get(`pokemon-form/${id}`),
+          apFetch.get(`stat`),
+        ]);
+        setData(pokemonResponse.data);
+
+        // Se necessário, você pode lidar com as informações de status aqui
+        const statusData = statusResponse.data.results;
+        console.log("Dados de status:", statusData);
       } catch (err) {
-        setError(err); // Define error como o objeto de erro
+        setError(err);
       } finally {
-          setLoading(false);
+        setLoading(false);
       }
     };
+
     fetchData();
-  }, [id]); // Adiciona "id" como dependência
+  }, [id]);
 
   if (loading) {
-      return <h3>Carregando...</h3>;
+    return <h3>Carregando...</h3>;
   }
-  
+
   if (error) {
     return <h1>Error: {error.message}</h1>;
   }
-  
-  const names = data.name; // Acesse o nome do Pokémon diretamente
-  const types = data.types; // Acesse a matriz de tipos
-  
+
+  const names = data.name;
+  const types = data.types;
   const sprite = data.sprites.front_default;
   const typeNames = types.map((typeData) => typeData.type.name);
-  
+
   return (
     <div>
       <p>{names}</p>
       <img src={sprite} alt={names} />
       <p>{typeNames.join(' | ')}</p>
-      <PokeInfo name={names}/>
+      <PokeInfo name={names} />
+      <PokeStatus />
     </div>
   );
-  
-  function PokeInfo({name}){
+}
 
-        const [pokemonAp, setPokemonAp] = useState({});
+function PokeInfo({ name }) {
+  const [pokemonAp, setPokemonAp] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-        useEffect(() => {
-            const fetchPokeAp = async () => {
-            try {
-                const res = await apFetch.get(`pokemon/${name}`);
-                setPokemonAp(res.data);
-            }catch (err) {
-                setError(err);
-            }finally {
-                setLoading(false);
-            }
-            };
-            fetchPokeAp();
-        }, [name]); 
+  useEffect(() => {
+    const fetchPokeAp = async () => {
+      try {
+        const res = await apFetch.get(`pokemon/${name}`);
+        setPokemonAp(res.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    const height = pokemonAp.height;
-    const weight = pokemonAp.weight;
-    const number = ("0000" + pokemonAp.id).slice(-4);;
-    
-    return(
-        <div>
-            <p>National №: #{number}</p>
-            <p>tamanho:{height}</p>
-            <p>peso:{weight}</p>
-        </div>
-    );
+    fetchPokeAp();
+  }, [name]);
+
+  if (loading) {
+    return <h3>Carregando informações adicionais...</h3>;
   }
+
+  if (error) {
+    return <h1>Error: {error.message}</h1>;
+  }
+
+  const height = pokemonAp.height;
+  const weight = pokemonAp.weight;
+  const number = ("0000" + pokemonAp.id).slice(-4);
+
+  return (
+    <div>
+      <p>National №: #{number}</p>
+      <p>Tamanho: {height}</p>
+      <p>Peso: {weight}</p>
+    </div>
+  );
+}
+
+function PokeStatus() {
+  const [nameStatus, setNameStatus] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPokeStatus = async () => {
+      try {
+        const res = await apFetch.get(`stat`);
+        setNameStatus(res.data.results);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPokeStatus();
+  }, []);
+
+  if (loading) {
+    return <h3>Carregando informações de status...</h3>;
+  }
+
+  if (error) {
+    return <h1>Error: {error.message}</h1>;
+  }
+
+  return (
+    <div>
+      <ul>
+        {nameStatus.map((statu, index) => (
+          <li key={index}>{statu.name}:</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Pokemon;
